87a2cdae2e825ce67527444bcbb1230ab5d00b6a
csc372 <141356425+csc372@users.noreply.github.com>
2026-03-08 19:34:32 -0400

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,12 @@
+<?php
+
+	/* TO-DO: Include session.php to handle login sessions
+              Hint: Both header.php and session.php are inside the includes folder
+    */
+
+
+
+	include 'includes/session.php';						// Include the session script
+	logout();											// Call the logout function to terminate session
+	header('Location: index.php');						// Redirect to index page
+?>
